a85f6867185cab7cd79eb5791a32dfaefcd225ac
Sovik Mandal <mandalsouvik9635@gmail.com>
2025-01-10 20:02:01 +0530

    Added Exercises-3

---
+++ Exercises/Exercises_3.js
@@ -0,0 +1,30 @@
+document.body.innerHTML = 'Exercises on String'
+
+'My Name is: '
+'My Name is: '
+'Souvik Mandal'
+'Souvik Mandal'
+'My Name is: ' + 'Souvik Mandal'
+'My Name is: Souvik Mandal'
+'Items('+ (1+1) +'): $' + (5 + 3)
+'Items(2): $8'
+`Items(${1 + 1}): $${5 + 3}`
+'Items(2): $8'
+alert(`Items(${1 + 1}): $${5 + 3}`);
+undefined
+'Items(' + (1 + 1) + '): $'+ (599 + 295)/100
+'Items(2): $8.94'
+`Items(${1 + 1}): $${(599 + 295)/100}`
+'Items(2): $8.94'
+alert(`Items(${1 + 1}): $${(599 + 295)/100}`);
+undefined
+alert('Total cost: $8.94\nThank you, come again');
+undefined
+`Items(${1 + 1 + 1 + 1}): $${(2*2095 + 499 + 2*799 + 499)/100}`
+'Items(4): $67.86'
+`Shipping & handling: $${(2*499)/100}`
+'Shipping & handling: $9.98'
+`Total Before tax: $${67.86}`
+'Total Before tax: $67.86'
+`Estimated tax(${10}%): $${(67860/10000)}`
+'Estimated tax(10%): $6.786'
