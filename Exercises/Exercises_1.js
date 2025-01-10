9a7d7db5e5561e536b88ee9de5b89b957f93c49e
Sovik Mandal <mandalsouvik9635@gmail.com>
2025-01-10 19:59:30 +0530

    Added Exercises-1

---
+++ Exercises/Exercises_1.js
@@ -0,0 +1,11 @@
+
+document.body.innerHTML = 'Souvik Mandal'
+'Souvik Mandal'
+document.body.innerHTML = 'Good Morning'
+'Good Morning'
+100-20-50+200
+230
+document.body.innerHTML = ''
+''
+10+8+20
+38
