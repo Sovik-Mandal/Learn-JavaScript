3b7be4a0cfb323b04be89f65c16126d6c2677a4e
Sovik Mandal <mandalsouvik9635@gamil.com>
2025-01-22 19:47:32 +0530

    Adding LocalStorage in cart

--- Amazon-Project/Scripts/amazon.js
+++ Amazon-Project/Scripts/amazon.js
@@ -1,14 +1,15 @@
-import {cart, addToCart} from '../data/cart.js'
-import { products } from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js';
+import {formatCurrency} from './utils/money.js';
 
-let productsHTML = ''
+let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
-      <div class="product-container">
+    <div class="product-container">
       <div class="product-image-container">
-        <img class="product-image"  src="${product.image}">
+        <img class="product-image"
+          src="${product.image}">
       </div>
 
       <div class="product-name limit-text-to-2-lines">
@@ -16,8 +17,8 @@ products.forEach((product) => {
       </div>
 
       <div class="product-rating-container">
-        <img class="product-rating-stars" src="Images/ratings/rating-${product.rating.stars * 10}.png">
-
+        <img class="product-rating-stars"
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
@@ -45,11 +46,12 @@ products.forEach((product) => {
       <div class="product-spacer"></div>
 
       <div class="added-to-cart">
-        <img src="Images/icons/checkmark.png">
+        <img src="images/icons/checkmark.png">
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+      <button class="add-to-cart-button button-primary js-add-to-cart"
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -60,12 +62,13 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
   let cartQuantity = 0;
-  cart.forEach((item) => {
-    cartQuantity += item.quantity;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
   });
 
-  document.querySelector('.js-cart-quantity') 
-  .innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart')
