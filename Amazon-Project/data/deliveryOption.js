94d8904454e4c9da410e4f6b1eef7173d6ac6a9d
Sovik Mandal <mandalsouvik9635@gamil.com>
2025-01-23 18:41:13 +0530

    update checkout.js section

---
+++ Amazon-Project/data/deliveryOption.js
@@ -0,0 +1,17 @@
+export const deliveryOption = [
+{
+  id: '1',
+  deliveryDate: 7,
+  priceCent: 0,
+},
+{
+  id: '2',
+  deliveryDate: 3,
+  priceCent: 499,
+},
+{
+  id: '3',
+  deliveryDate: 1,
+  priceCent: 999,
+},
+]
